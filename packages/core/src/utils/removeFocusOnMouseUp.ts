dd7d8efb2d370acb6c4b7df50a9457ccea018ea4
Stefanie Hatcher <stefhatcher@users.noreply.github.com>
2019-05-01 09:27:02 -0700

    fix(removeFocusOnMouseUp): Remove focus from currentTarget. (#53)

--- packages/core/src/utils/removeFocusOnMouseUp.ts
+++ packages/core/src/utils/removeFocusOnMouseUp.ts
@@ -2,7 +2,7 @@
 export default function removeFocusOnMouseUp<T>(event: React.MouseEvent<T>) {
   // This event takes too long, and prevents onClick occasionally.
   // And we do not need to do it syncronously.
-  const { target } = event;
+  const { currentTarget, target } = event;
 
-  window.setTimeout(() => (target as HTMLElement).blur(), 0);
+  window.setTimeout(() => ((currentTarget || target) as HTMLElement).blur(), 0);
 }
