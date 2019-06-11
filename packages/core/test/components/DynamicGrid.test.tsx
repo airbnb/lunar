f8d79a826c8043826760590bd46fdbd39213681d
kyle_schiller <kyle_schiller@kyleschllersmbp.airgc.net>
2019-06-11 13:13:08 -0700

    add tests

---
+++ packages/core/test/components/DynamicGrid.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import DynamicGrid from '../../src/components/DynamicGrid';
+
+describe('<DynamicGrid />', () => {
+  it('renders an DynamicGrid with items inside', () => {
+    const wrapper = shallow(
+      <DynamicGrid>
+        <div>Item</div>
+        <div>Item</div>
+        <div>Item</div>
+      </DynamicGrid>,
+    ).dive();
+
+    expect(wrapper.children()).toHaveLength(3);
+  });
+  it('renders an DynamicGrid with full width items', () => {
+    const wrapper = shallow(
+      <DynamicGrid>
+        <div>Item</div>
+        <div>Item</div>
+        <div>Item</div>
+      </DynamicGrid>,
+    ).dive();
+
+    expect(wrapper.childAt(0).prop('style').width).toBe('100%');
+  });
+  it('renders an DynamicGrid with 4 items per row', () => {
+    const wrapper = shallow(
+      <DynamicGrid defaultItems={4}>
+        <div>Item</div>
+        <div>Item</div>
+        <div>Item</div>
+      </DynamicGrid>,
+    ).dive();
+
+    expect(wrapper.childAt(0).prop('style').width).toBe('25%');
+  });
+});
