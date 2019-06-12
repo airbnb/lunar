cdf825638205a8fe00f9f23fa0cc4b11046e5342
kyle_schiller <kyle_schiller@kyle-schillers-MacBook-Pro.local>
2019-06-11 20:29:10 -0700

    commit

--- packages/layouts/src/components/SidePanel/index.tsx
+++ packages/layouts/src/components/SidePanel/index.tsx
@@ -1,15 +1,12 @@
-import React from "react";
-import withStyles, {
-  css,
-  WithStylesProps
-} from "@airbnb/lunar/lib/composers/withStyles";
-import PropTypes from "prop-types";
-import { mutuallyExclusiveProps } from "airbnb-prop-types";
-import IconChevronLeft from "@airbnb/lunar-icons/lib/interface/IconChevronLeft";
-import IconChevronRight from "@airbnb/lunar-icons/lib/interface/IconChevronRight";
-import Spacing from "@airbnb/lunar/lib/components/Spacing";
+import React from 'react';
+import withStyles, { css, WithStylesProps } from '@airbnb/lunar/lib/composers/withStyles';
+import PropTypes from 'prop-types';
+import { mutuallyExclusiveProps } from 'airbnb-prop-types';
+import IconChevronLeft from '@airbnb/lunar-icons/lib/interface/IconChevronLeft';
+import IconChevronRight from '@airbnb/lunar-icons/lib/interface/IconChevronRight';
+import Spacing from '@airbnb/lunar/lib/components/Spacing';
 
-import iconComponent from "../../prop-types/iconComponent";
+import iconComponent from '@airbnb/lunar/lib/prop-types/iconComponent';
 
 export type SplitPaneProps = {
   sidePane: React.ReactNode;
@@ -34,53 +31,50 @@ export type SplitPaneState = {
 };
 
 /** A symmetrical two-column layout with optional top and side navigation. */
-class SidePanel extends React.Component<
-  SplitPaneProps & WithStylesProps,
-  SplitPaneState
-> {
+class SidePanel extends React.Component<SplitPaneProps & WithStylesProps, SplitPaneState> {
   static propTypes = {
     minWidth: mutuallyExclusiveProps(
       PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-      "fixedWidth"
+      'fixedWidth',
     ),
     maxWidth: mutuallyExclusiveProps(
       PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-      "fixedWidth"
+      'fixedWidth',
     ),
     fixedWidth: mutuallyExclusiveProps(
       PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-      "maxWidth",
-      "minWidth",
-      "percentWidth"
+      'maxWidth',
+      'minWidth',
+      'percentWidth',
     ),
-    percentWidth: mutuallyExclusiveProps(PropTypes.number, "fixedWidth")
+    percentWidth: mutuallyExclusiveProps(PropTypes.number, 'fixedWidth'),
   };
 
   public static defaultProps = {
-    background: "white",
+    background: 'white',
     buttonTop: 16,
     collapsible: true,
     compact: true,
-    iconColor: "#484848", //core.neutral[5]
+    iconColor: '#484848', //core.neutral[5]
     iconClosed: IconChevronRight,
     iconOpen: IconChevronLeft,
-    iconSize: "1.1rem",
+    iconSize: '1.1rem',
     maxWidth: Infinity,
     minWidth: 0,
-    rightSide: false
+    rightSide: false,
   };
 
   constructor(props: SplitPaneProps & WithStylesProps) {
     super(props);
 
     this.state = {
-      collapsed: false
+      collapsed: false,
     };
   }
 
   private toggleSidePane = () => {
     this.setState(({ collapsed }) => ({
-      collapsed: !collapsed
+      collapsed: !collapsed,
     }));
   };
 
@@ -102,17 +96,13 @@ class SidePanel extends React.Component<
       minWidth,
       maxWidth,
       fixedWidth,
-      rightSide
+      rightSide,
     } = this.props;
     const { collapsed } = this.state;
 
-    const defaultWidth = percentWidth
-      ? `${percentWidth}%`
-      : `${40 * theme!.unit}px`;
+    const defaultWidth = percentWidth ? `${percentWidth}%` : `${40 * theme!.unit}px`;
 
-    const buttonBackgroundColor = collapsed
-      ? theme!.color.accent.bg
-      : background;
+    const buttonBackgroundColor = collapsed ? theme!.color.accent.bg : background;
 
     const sidePanelStyle = {
       width: collapsed ? 0 : fixedWidth || defaultWidth,
@@ -123,13 +113,9 @@ class SidePanel extends React.Component<
         : `0 0 ${collapsed ? 0 : defaultWidth}`,
       padding: collapsed ? 0 : 3 * theme!.unit,
       borderRightColor:
-        !rightSide && collapsed
-          ? buttonBackgroundColor
-          : theme!.color.core.neutral[1],
+        !rightSide && collapsed ? buttonBackgroundColor : theme!.color.core.neutral[1],
       borderLeftColor:
-        rightSide && collapsed
-          ? buttonBackgroundColor
-          : theme!.color.core.neutral[1]
+        rightSide && collapsed ? buttonBackgroundColor : theme!.color.core.neutral[1],
     };
 
     const Icon = collapsed ? iconClosed : iconOpen;
@@ -138,17 +124,13 @@ class SidePanel extends React.Component<
       top: buttonTop,
       background: buttonBackgroundColor,
       borderRightColor:
-        !rightSide && !collapsed
-          ? buttonBackgroundColor
-          : theme!.color.core.neutral[1],
+        !rightSide && !collapsed ? buttonBackgroundColor : theme!.color.core.neutral[1],
       borderLeftColor:
-        rightSide && !collapsed
-          ? buttonBackgroundColor
-          : theme!.color.core.neutral[1]
+        rightSide && !collapsed ? buttonBackgroundColor : theme!.color.core.neutral[1],
     };
 
     const mainPanelColorStyle = {
-      background: background || theme!.color.accent.bg
+      background: background || theme!.color.accent.bg,
     };
 
     const collapseButton = (
@@ -160,7 +142,7 @@ class SidePanel extends React.Component<
           collapsed && !rightSide && styles.collapseButtonHiddenLeft,
           collapsed && rightSide && styles.collapseButtonHiddenRight,
           !collapsed && !rightSide && styles.collapseButtonVisibleLeft,
-          !collapsed && rightSide && styles.collapseButtonVisibleRight
+          !collapsed && rightSide && styles.collapseButtonVisibleRight,
         )}
         onClick={this.toggleSidePane}
       >
@@ -172,23 +154,19 @@ class SidePanel extends React.Component<
 
     return (
       <div {...css(styles.splitPane)}>
-        {rightSide && (
-          <div {...css(styles.mainPanel, mainPanelColorStyle)}>{mainPane}</div>
-        )}
+        {rightSide && <div {...css(styles.mainPanel, mainPanelColorStyle)}>{mainPane}</div>}
         <div
           {...css(
             styles.sidePanel,
             rightSide && styles.sidePanelRight,
             !rightSide && styles.sidePanelLeft,
-            sidePanelStyle
+            sidePanelStyle,
           )}
         >
           {collapsible && collapseButton}
           <div {...css(styles.sidePanelInner)}>{!collapsed && sidePane}</div>
         </div>
-        {!rightSide && (
-          <div {...css(styles.mainPanel, mainPanelColorStyle)}>{mainPane}</div>
-        )}
+        {!rightSide && <div {...css(styles.mainPanel, mainPanelColorStyle)}>{mainPane}</div>}
       </div>
     );
   }
@@ -197,72 +175,72 @@ class SidePanel extends React.Component<
 export default withStyles(
   ({ unit, color }) => ({
     splitPane: {
-      display: "inline-flex",
+      display: 'inline-flex',
       minHeight: `calc(100vh - ${8 * unit}px)`,
-      width: "100%"
+      width: '100%',
     },
     mainPanel: {
       padding: unit * 4,
-      overflow: "auto",
-      flexGrow: 1
+      overflow: 'auto',
+      flexGrow: 1,
     },
     sidePanel: {
-      wordBreak: "break-word",
+      wordBreak: 'break-word',
       background: color.accent.bg,
       height: `calc(100vh - ${8 * unit}px)`,
-      position: "sticky",
-      top: 8 * unit
+      position: 'sticky',
+      top: 8 * unit,
     },
 
     sidePanelLeft: {
-      borderRight: "1px solid"
+      borderRight: '1px solid',
     },
 
     sidePanelRight: {
-      borderLeft: "1px solid"
+      borderLeft: '1px solid',
     },
 
     sidePanelInner: {
-      height: "100%",
-      overflow: "auto",
-      zIndex: 100
+      height: '100%',
+      overflow: 'auto',
+      zIndex: 100,
     },
 
     collapseButton: {
-      cursor: "pointer",
-      position: "absolute",
+      cursor: 'pointer',
+      position: 'absolute',
       border: `1px solid ${color.core.neutral[1]}`,
       padding: 0,
-      borderRadius: 0
+      borderRadius: 0,
     },
 
     collapseButtonVisibleRight: {
       left: -1,
       borderBottomRightRadius: 4,
       borderTopRightRadius: 4,
-      borderLeft: "1px solid"
+      borderLeft: '1px solid',
     },
     collapseButtonHiddenRight: {
       right: 0,
       borderBottomLeftRadius: 4,
       borderTopLeftRadius: 4,
-      borderRight: "1px solid"
+      borderRight: '1px solid',
     },
 
     collapseButtonVisibleLeft: {
       right: -1,
       borderBottomLeftRadius: 4,
       borderTopLeftRadius: 4,
-      borderRight: "1px solid"
+      borderRight: '1px solid',
     },
     collapseButtonHiddenLeft: {
       left: 0,
       borderBottomRightRadius: 4,
       borderTopRightRadius: 4,
-      borderLeft: "1px solid"
-    }
+      borderLeft: '1px solid',
+    },
   }),
   {
-    passThemeProp: true
-  }
+    passThemeProp: true,
+  },
 )(SidePanel);
