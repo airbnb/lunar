37eff146f0095de6cc9dd63dbe2ebc9adc8e8520
Miles Johnson <miles.johnson@airbnb.com>
2019-06-18 15:13:10 -0700

    Setup contexts.

--- packages/core/src/providers/DirectionProvider.tsx
+++ packages/core/src/providers/DirectionProvider.tsx
@@ -1,5 +1,20 @@
-import { DirectionProvider, DirectionContext } from 'aesthetic-react';
+import React from 'react';
+import {
+  DirectionProvider as BaseDirectionProvider,
+  DirectionProviderProps,
+  DirectionContext,
+} from 'aesthetic-react';
+import { Omit } from 'utility-types';
+import Core from '..';
 
 export { DirectionContext };
 
-export default DirectionProvider;
+export type Props = Omit<DirectionProviderProps, 'aesthetic'>;
+
+export default function DirectionProvider({ children, ...props }: Props) {
+  return (
+    <BaseDirectionProvider aesthetic={Core.aesthetic} {...props}>
+      {children}
+    </BaseDirectionProvider>
+  );
+}
