97c152aa59c6aea46d22b098137d24455ec34502
Miles Johnson <miles.johnson@airbnb.com>
2019-06-20 15:50:17 -0700

    Do S.

--- packages/core/test/components/Spacing.test.tsx
+++ packages/core/test/components/Spacing.test.tsx
@@ -1,51 +1,53 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallowWithStyles } from '@airbnb/lunar-test-utils';
 import Spacing from '../../src/components/Spacing';
 
 describe('<Spacing />', () => {
   it('renders with a side', () => {
-    const wrapper = shallow(<Spacing top={1}>Content</Spacing>).dive();
+    const wrapper = shallowWithStyles(<Spacing top={1}>Content</Spacing>);
 
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders with many sides', () => {
-    const wrapper = shallow(
+    const wrapper = shallowWithStyles(
       <Spacing top={1} bottom={3} left={2} right={4}>
         Content
       </Spacing>,
-    ).dive();
+    );
 
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders as inline', () => {
-    const wrapper = shallow(
+    const wrapper = shallowWithStyles(
       <Spacing vertical={2} inline>
         Content
       </Spacing>,
-    ).dive();
+    );
 
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders with padding', () => {
-    const wrapper = shallow(
+    const wrapper = shallowWithStyles(
       <Spacing all={2} inner>
         Content
       </Spacing>,
-    ).dive();
+    );
 
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders a <div /> by default', () => {
-    const wrapper = shallow(<Spacing>Content</Spacing>).dive();
+    const wrapper = shallowWithStyles(<Spacing>Content</Spacing>);
+
     expect(wrapper.type()).toEqual('div');
   });
 
   it('renders with the passed in tag type', () => {
-    const wrapper = shallow(<Spacing tag="footer">Footer content</Spacing>).dive();
+    const wrapper = shallowWithStyles(<Spacing tag="footer">Footer content</Spacing>);
+
     expect(wrapper.type()).toEqual('footer');
   });
 });
