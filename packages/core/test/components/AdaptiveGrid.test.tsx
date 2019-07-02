aee9ad1c4cae5337c6f762bed240b43fde415b02
kyle_schiller <kyle_schiller@kyleschllersmbp.airgc.net>
2019-07-02 13:44:42 -0700

    fix tests

--- packages/core/test/components/AdaptiveGrid.test.tsx
+++ packages/core/test/components/AdaptiveGrid.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import AdaptiveGrid from '../../src/components/AdaptiveGrid';
 
 describe('<AdaptiveGrid />', () => {
@@ -13,5 +13,20 @@ describe('<AdaptiveGrid />', () => {
     ).dive();
 
     expect(wrapper.children()).toHaveLength(3);
+  });
+  it('renders an AdaptiveGrid and passes props', () => {
+    const wrapper = mount(
+      <AdaptiveGrid breakpoints={{ 1200: 3 }} defaultItemsPerRow={2}>
+        <div>Item</div>
+        <div>Item</div>
+        <div>Item</div>
+      </AdaptiveGrid>,
+    );
+
+    const breakpoints = wrapper.childAt(0).prop('breakpoints');
+    const defaultItemsPerRow = wrapper.childAt(0).prop('defaultItemsPerRow');
+
+    expect(breakpoints[1200]).toEqual(3);
+    expect(defaultItemsPerRow).toEqual(2);
   });
 });
