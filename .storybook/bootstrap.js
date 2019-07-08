45cb3cdce9af92dc32c5d84b378bcc41cc3ce74a
Miles Johnson <miles.johnson@airbnb.com>
2019-07-08 10:08:47 -0700

    Remove backgrounds addon.

---
+++ .storybook/bootstrap.js
@@ -0,0 +1,13 @@
+import 'airbnb-js-shims';
+import { stripHexcode } from 'emojibase';
+import Lunar from '@airbnb/lunar';
+
+Lunar.initialize({
+  name: 'Lunar',
+  emojiCDN: (hexcode, large) =>
+    `https://cdn.jsdelivr.net/emojione/assets/4.5/png/${large ? 64 : 32}/${stripHexcode(
+      hexcode,
+    ).toLowerCase()}.png`,
+  logger: console.log,
+  theme: 'light',
+});
