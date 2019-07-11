0836eb66ef9135f03698b9a9b5b549fa20a4f262
Miles Johnson <miles.johnson@airbnb.com>
2019-07-11 11:46:18 -0700

    Fix typo.

--- packages/app-shell/test/components/Layout.test.tsx
+++ packages/app-shell/test/components/Layout.test.tsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import Layout from '../../src/components/Layout';
 
 describe('Layout', () => {
-  it('renderss', () => {
+  it('renders', () => {
     const wrapper = shallowWithStyles(<Layout>Child</Layout>);
 
     expect(wrapper.isEmptyRender()).toBe(false);
