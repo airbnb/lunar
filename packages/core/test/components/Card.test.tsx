3da4af74654017feea5a3fe07eaeb8e608b2eddb
Stefanie Hatcher <stefhatcher@users.noreply.github.com>
2019-07-11 15:29:28 -0700

    update(Card): Add overflow prop for overflow visible. (#116)

--- packages/core/test/components/Card.test.tsx
+++ packages/core/test/components/Card.test.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Card from '../../src/components/Card';
+import Card, { Content } from '../../src/components/Card';
 
 describe('<Card />', () => {
   it('errors if `Content` child is not passed', () => {
     expect(() => {
       shallow(<Card>Invalid</Card>);
     }).toThrowError();
+  });
+
+  it('it renders different styles for `overflow`', () => {
+    const withoutOverflow = shallow(
+      <Card>
+        <Content>Foo</Content>
+      </Card>,
+    );
+
+    const withOverflow = shallow(
+      <Card overflow>
+        <Content>Foo</Content>
+      </Card>,
+    );
+
+    expect(withoutOverflow.prop('className')).not.toBe(withOverflow.prop('className'));
   });
 });
