21bc0b60cfc3fd95ba92d320d430cc2c7831185b
kyle_schiller <kyle_schiller@kyle-schillers-MacBook-Pro.local>
2019-07-22 14:16:26 -0700

    update(Divider): add short prop

--- packages/core/src/components/Divider/index.tsx
+++ packages/core/src/components/Divider/index.tsx
@@ -7,6 +7,8 @@ export type Props = {
   bottom?: SpacingRange;
   /** Spacing on the top. */
   top?: SpacingRange;
+  /** Render the divider with a short width. */
+  short?: boolean;
 };
 
 /** A horizontal divider. */
@@ -14,21 +16,25 @@ export class Divider extends React.Component<Props & WithStylesProps> {
   static defaultProps: Props = {
     bottom: 2,
     top: 2,
+    short: false,
   };
 
   render() {
-    const { cx, styles, bottom, top } = this.props;
+    const { cx, styles, bottom, short, top } = this.props;
 
     return (
       <Spacing bottom={bottom} top={top}>
-        <div className={cx(styles.divider)} />
+        <div className={cx(styles.divider, short && styles.divider_short)} />
       </Spacing>
     );
   }
 }
 
-export default withStyles(({ ui }) => ({
+export default withStyles(({ ui, unit }) => ({
   divider: {
     borderBottom: ui.border,
+  },
+  divider_short: {
+    width: 4 * unit,
   },
 }))(Divider);
