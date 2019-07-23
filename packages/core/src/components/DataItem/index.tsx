55cc23c6d0f69d74fbb6210d4bbd3fc6507511bb
kyle_schiller <kyle_schiller@kyle-schillers-MacBook-Pro.local>
2019-07-23 16:18:18 -0700

    use flexbox over float, endAlign instead of floatLinkRight

--- packages/core/src/components/DataItem/index.tsx
+++ packages/core/src/components/DataItem/index.tsx
@@ -6,7 +6,7 @@ import Text from '../Text';
 function DataItem({
   label,
   link,
-  floatLinkRight,
+  endAlign,
   inline,
   children,
   cx,
@@ -14,35 +14,41 @@ function DataItem({
 }: {
   label: string;
   link?: NonNullable<React.ReactNode>;
-  floatLinkRight?: boolean;
+  endAlign?: boolean;
   inline?: boolean;
   children: NonNullable<React.ReactNode>;
 } & WithStylesProps) {
   return (
-    <div className={cx(inline && styles.data_item_inline)}>
-      <Text inline small muted>
-        {label}
-      </Text>
-      {link && (
-        <div className={cx(floatLinkRight && styles.link_float_right, styles.link)}>
-          <Text inline small muted>
+    <div className={cx(inline && styles.inline)}>
+      <div className={cx(styles.wrapper, endAlign && styles.wrapper_endAlign)}>
+        <Spacing right={1}>
+          <Text small muted>
+            {label}
+          </Text>
+        </Spacing>
+
+        {link && (
+          <Text small muted>
             {link}
           </Text>
-        </div>
-      )}
+        )}
+      </div>
+
       <Text>{children}</Text>
     </div>
   );
 }
 
 export default withStyles(() => ({
-  data_item_inline: {
+  inline: {
     display: 'inline-block',
   },
-  link_float_right: {
-    float: 'right',
+
+  wrapper: {
+    display: 'flex',
   },
-  link: {
-    display: 'inline-block',
+
+  wrapper_endAlign: {
+    justifyContent: 'space-between',
   },
 }))(DataItem);
