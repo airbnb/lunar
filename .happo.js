83d55a50d6892ea143d658f400cea3230578c744
Miles Johnson <miles.johnson@airbnb.com>
2019-08-02 10:35:54 -0700

    Finalize config.

--- .happo.js
+++ .happo.js
@@ -4,15 +4,14 @@ const storybook = require('happo-plugin-storybook');
 module.exports = {
   apiKey: process.env.HAPPO_API_KEY,
   apiSecret: process.env.HAPPO_API_SECRET,
-  type: 'react',
 
   targets: {
     'chrome-desktop': new RemoteBrowserTarget('chrome', {
       viewport: '1024x768',
     }),
-    // 'chrome-mobile': new RemoteBrowserTarget('chrome', {
-    //   viewport: '320x640',
-    // }),
+    'firefox-desktop': new RemoteBrowserTarget('firefox', {
+      viewport: '1024x768',
+    }),
   },
 
   plugins: [
