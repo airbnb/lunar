6ef2f2c556da09fedf8c7be350f6ff3c4eca8758
austin_piel <austin.piel@airbnb.com>
2019-08-15 13:58:29 -0700

    Change to use local images

--- packages/core/src/components/Image.story.tsx
+++ packages/core/src/components/Image.story.tsx
@@ -1,76 +1,31 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Image from './Image';
+import stars from ':storybook/images/stars.jpg';
 
 storiesOf('Core/Image', module)
   .addParameters({
     inspectComponents: [Image],
   })
-  .add('default', () => (
-    <Image
-      src="https://images.freeimages.com/images/large-previews/035/young-golden-retriever-1404848.jpg"
-      alt="Something descriptive"
-      height={450}
-    />
-  ))
+  .add('default', () => <Image src={stars} alt="Something descriptive" height={600} />)
   .add('background', () => (
-    <Image
-      background
-      src="https://images.freeimages.com/images/large-previews/035/young-golden-retriever-1404848.jpg"
-      alt="Something descriptive"
-      height={450}
-    />
+    <Image background src={stars} alt="Something descriptive" height={600} />
   ))
   .add('background contain', () => (
-    <Image
-      background
-      contain
-      src="https://images.freeimages.com/images/large-previews/035/young-golden-retriever-1404848.jpg"
-      alt="Something descriptive"
-      height={450}
-    />
+    <Image background contain src={stars} alt="Something descriptive" height={600} />
   ))
   .add('no background, still contain, default borderRadius', () => (
-    <Image
-      contain
-      src="https://images.freeimages.com/images/large-previews/035/young-golden-retriever-1404848.jpg"
-      alt="Something descriptive"
-      height={450}
-    />
+    <Image contain src={stars} alt="Something descriptive" height={600} />
   ))
   .add('with custom borderRadius', () => (
-    <Image
-      contain
-      borderRadius={25}
-      src="https://images.freeimages.com/images/large-previews/035/young-golden-retriever-1404848.jpg"
-      alt="Something descriptive"
-      height={450}
-    />
+    <Image contain borderRadius={25} src={stars} alt="Something descriptive" height={600} />
   ))
   .add('with zero borderRadius', () => (
-    <Image
-      contain
-      borderRadius={0}
-      src="https://images.freeimages.com/images/large-previews/035/young-golden-retriever-1404848.jpg"
-      alt="Something descriptive"
-      height={450}
-    />
+    <Image contain borderRadius={0} src={stars} alt="Something descriptive" height={600} />
   ))
-  .add('background, top', () => (
-    <Image
-      alignTop
-      background
-      src="https://images.freeimages.com/images/large-previews/035/young-golden-retriever-1404848.jpg"
-      alt="Something descriptive"
-      height={450}
-    />
+  .add('background alignTop', () => (
+    <Image alignTop background src={stars} alt="Something descriptive" height={600} />
   ))
-  .add('background, bottom', () => (
-    <Image
-      alignBottom
-      background
-      src="https://images.freeimages.com/images/large-previews/035/young-golden-retriever-1404848.jpg"
-      alt="Something descriptive"
-      height={450}
-    />
+  .add('background, alignBottom', () => (
+    <Image alignBottom background src={stars} alt="Something descriptive" height={600} />
   ));
--- packages/core/test/components/Image.test.tsx
+++ packages/core/test/components/Image.test.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Image from '../../src/components/Image';
 
-const url =
-  'https://images.freeimages.com/images/large-previews/035/young-golden-retriever-1404848.jpg';
-
 describe('<Image />', () => {
   it('renders default', () => {
-    const wrapper = shallow(<Image src={url} alt="Test" />);
+    const wrapper = shallow(<Image src="src" alt="Test" />);
 
     expect(wrapper.type()).toBe('div');
     expect(wrapper.childAt(0).type()).toBe('img');
@@ -16,21 +13,21 @@ describe('<Image />', () => {
   });
 
   it('has custom height/width', () => {
-    const wrapper = shallow(<Image src={url} alt="Test" height={200} width={400} />);
+    const wrapper = shallow(<Image src="src" alt="Test" height={200} width={400} />);
 
     expect(wrapper.childAt(0).prop('height')).toBe(200);
     expect(wrapper.childAt(0).prop('width')).toBe(400);
   });
 
   it('is background image', () => {
-    const wrapper = shallow(<Image background src={url} alt="Test" />);
+    const wrapper = shallow(<Image background src="src" alt="Test" />);
 
     expect(wrapper.childAt(0).type()).toBe('div');
     expect(wrapper.childAt(0).prop('className')).toMatch('background');
   });
 
   it('pass cover/contain to background image', () => {
-    const wrapper = shallow(<Image background contain src={url} alt="Test" />);
+    const wrapper = shallow(<Image background contain src="src" alt="Test" />);
     expect(wrapper.childAt(0).prop('className')).toMatch('contain');
 
     wrapper.setProps({ contain: false, cover: true });
@@ -38,6 +35,6 @@ describe('<Image />', () => {
   });
 
   it('throws error if cover and contain are both true', () => {
-    expect(() => shallow(<Image background contain cover src={url} alt="Test" />)).toThrowError();
+    expect(() => shallow(<Image background contain cover src="src" alt="Test" />)).toThrowError();
   });
 });
