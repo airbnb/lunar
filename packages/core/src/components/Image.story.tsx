6f6868b25b82af67af8187f6c7edcf352b5b4968
austin_piel <austin.piel@airbnb.com>
2019-08-20 13:49:20 -0700

    Use lunar image instead of stars

--- packages/core/src/components/Image.story.tsx
+++ packages/core/src/components/Image.story.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Image from './Image';
-import stars from ':storybook/images/stars.jpg';
+import lunar from ':storybook/images/lunar-logo.png';
 
 storiesOf('Core/Image', module)
   .addParameters({
     inspectComponents: [Image],
   })
-  .add('default', () => <Image src={stars} alt="Something descriptive" height={600} />)
+  .add('default', () => <Image src={lunar} alt="Something descriptive" height={600} />)
   .add('background', () => (
-    <Image background src={stars} alt="Something descriptive" height={600} />
+    <Image background src={lunar} alt="Something descriptive" height={600} />
   ))
   .add('background contain', () => (
-    <Image background contain src={stars} alt="Something descriptive" height={600} />
+    <Image background contain src={lunar} alt="Something descriptive" height={600} />
   ))
   .add('no background, still contain, default borderRadius', () => (
-    <Image contain src={stars} alt="Something descriptive" height={600} />
+    <Image contain src={lunar} alt="Something descriptive" height={600} />
   ))
   .add('with custom borderRadius', () => (
-    <Image contain borderRadius={25} src={stars} alt="Something descriptive" height={600} />
+    <Image contain borderRadius={25} src={lunar} alt="Something descriptive" height={600} />
   ))
   .add('with zero borderRadius', () => (
-    <Image contain borderRadius={0} src={stars} alt="Something descriptive" height={600} />
+    <Image contain borderRadius={0} src={lunar} alt="Something descriptive" height={600} />
   ))
   .add('background alignTop', () => (
-    <Image alignTop background src={stars} alt="Something descriptive" height={600} />
+    <Image alignTop background src={lunar} alt="Something descriptive" height={600} />
   ))
   .add('background, alignBottom', () => (
-    <Image alignBottom background src={stars} alt="Something descriptive" height={600} />
+    <Image alignBottom background src={lunar} alt="Something descriptive" height={600} />
   ));
