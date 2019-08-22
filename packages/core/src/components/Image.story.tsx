8ab5021a5586124d08a540f04715c4b9018affba
austin_piel <austin.piel@airbnb.com>
2019-08-22 11:54:16 -0700

    Add Happo delay

--- packages/core/src/components/Image.story.tsx
+++ packages/core/src/components/Image.story.tsx
@@ -6,6 +6,7 @@ import lunar from ':storybook/images/lunar-logo.png';
 storiesOf('Core/Image', module)
   .addParameters({
     inspectComponents: [Image],
+    happo: { delay: 250 },
   })
   .add('default', () => <Image src={lunar} alt="Something descriptive" height={600} />)
   .add('background', () => (
