f7230730adf1cf219fd687ad8a37a24aeb6325b0
Adam Glynn-Finnegan <aglynnfinn@gmail.com>
2019-08-23 14:20:46 -0700

    Redo the things.

--- packages/icons/src/general/IconNotes.tsx
+++ packages/icons/src/general/IconNotes.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import withIcon, { Props } from '../withIcon';
 
 function IconNotes(props: Props) {
-  return <svg {...props} viewBox="0 0 24 24"><path d="M3 18h12v-2H3v2zM3 6v2h18V6H3zm0 7h18v-2H3v2z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>;
+  return (
+    <svg {...props} viewBox="0 0 24 24">
+      <path d="M3 18h12v-2H3v2zM3 6v2h18V6H3zm0 7h18v-2H3v2z" />
+      <path fill="none" d="M0 0h24v24H0V0z" />
+    </svg>
+  );
 }
 
 export default withIcon('IconNotes')(IconNotes);
