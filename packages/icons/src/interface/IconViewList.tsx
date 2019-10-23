baeefdddb79539904a923900bb2c30f2287eae08
Tiffany Ng <tyn2@users.noreply.github.com>
2019-10-22 17:24:12 -0700

    new(Icons): Add IconVariable, IconViewList, IconZoomToFit icons. (#217)

---
+++ packages/icons/src/interface/IconViewList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import withIcon, { Props } from '../withIcon';
+
+function IconViewList(props: Props) {
+  return (
+    <svg {...props} viewBox="0 0 24 24">
+      <path d="M4 14h4v-4H4v4zm0 5h4v-4H4v4zM4 9h4V5H4v4zm5 5h12v-4H9v4zm0 5h12v-4H9v4zM9 5v4h12V5H9z" />
+    </svg>
+  );
+}
+
+export default withIcon('IconViewList')(IconViewList);
