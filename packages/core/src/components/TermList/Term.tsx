5abc28fab26f436449a2388d4b82dce290bdc4f8
Kyle Schiller <kyleschiller@gmail.com>
2019-11-15 16:18:17 -0800

    put descriptions in the right place

--- packages/core/src/components/TermList/Term.tsx
+++ packages/core/src/components/TermList/Term.tsx
@@ -5,24 +5,18 @@ import Spacing from '../Spacing';
 import Text from '../Text';
 
 export type Props = {
+  /** Term label describing the value displayed. */
   label: string;
+  /** Detail links to go after Term label. */
   after?: React.ReactNode;
+  /** If enabled, detail links at aligned to the right end. */
   endAlign?: boolean;
+  /** If enabled, term label is regular size instead of small. */
   regular?: boolean;
   children: NonNullable<React.ReactNode>;
 };
 
-export default function Term({
-  /** Term label describing the value displayed. */
-  label,
-  /** Detail links to go after Term label. */
-  after,
-  /** If enabled, detail links at aligned to the right end. */
-  endAlign,
-  /** If enabled, term label is regular size instead of small. */
-  regular,
-  children,
-}: Props) {
+export default function Term({ label, after, endAlign, regular, children }: Props) {
   return (
     <>
       <dt>
