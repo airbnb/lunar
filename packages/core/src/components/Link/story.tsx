cad9f3803a075d3168440b1523e1cfb596fa069f
austin_piel <austin.piel@airbnb.com>
2019-11-18 19:06:48 -0800

    Add micro prop

--- packages/core/src/components/Link/story.tsx
+++ packages/core/src/components/Link/story.tsx
@@ -27,9 +27,12 @@ aButtonLink.story = {
   name: 'A button link.',
 };
 
-export function withDifferentSizingSmallRegularDefaultAndLarge() {
+export function withDifferentSizingMicroSmallRegularDefaultAndLarge() {
   return (
     <>
+      <Link micro href="https://github.com/airbnb/lunar">
+        Link
+      </Link>{' '}
       <Link small href="https://github.com/airbnb/lunar">
         Link
       </Link>{' '}
@@ -41,7 +44,7 @@ export function withDifferentSizingSmallRegularDefaultAndLarge() {
   );
 }
 
-withDifferentSizingSmallRegularDefaultAndLarge.story = {
+withDifferentSizingMicroSmallRegularDefaultAndLarge.story = {
   name: 'With different sizing: small, regular (default), and large.',
 };
 
