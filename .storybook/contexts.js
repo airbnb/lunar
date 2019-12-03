dc7deb2673fe6e99b79dae858ce256332f5e52b2
Miles Johnson <miles.johnson@airbnb.com>
2019-12-03 13:54:25 -0800

    Update context.

--- .storybook/contexts.js
+++ .storybook/contexts.js
@@ -7,8 +7,12 @@ export default [
     title: 'Change the theme',
     components: [ThemeProvider],
     params: [
-      { name: 'Light', props: { name: 'light', propagate: true, onClick: action }, default: true },
-      { name: 'Dark', props: { name: 'dark', propagate: true, onClick: action } },
+      {
+        name: 'Light',
+        props: { name: 'light', propagate: true },
+        default: true,
+      },
+      { name: 'Dark', props: { name: 'dark', propagate: true } },
     ],
   },
   {
