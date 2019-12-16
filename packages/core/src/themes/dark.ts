a00805e3c8d0a13cca05115c289ce44981c932d9
Adam GF <aglynnfinn@gmail.com>
2019-12-16 14:20:02 -0800

    style: Minor polish to dark mode. (#266)

--- packages/core/src/themes/dark.ts
+++ packages/core/src/themes/dark.ts
@@ -8,13 +8,13 @@ const brand: Theme['color']['brand'] = {
 
 const color: Theme['color']['core'] = {
   // Gray
-  neutral: ['#2F2F2F', '#373737', '#474747', '#777777', '#B1B1B1', '#D1D1D1', '#F0F0F0'],
+  neutral: ['#242424', '#373737', '#474747', '#777777', '#B1B1B1', '#D1D1D1', '#F0F0F0'],
   // Blue
   secondary: ['#073260', '#0A437F', '#105094', '#1561B2', '#237CDA', '#8AB0D9', '#B9D0E8'],
   // Any
   primary: ['#38108A', '#41159C', '#5A1FD1', '#9362F9', '#AD87FF', '#CDBCFF', '#DACDFF'],
   // Green
-  success: ['#0E6208', '#1C7315', '#227D1B', '#4AA83E', '#5ED24F', '#328D28', '#227D1B'],
+  success: ['#0E6208', '#1C7315', '#227D1B', '#4AA83E', '#5ED24F', '#9CE592', '#227D1B'],
   // Yellow/Orange
   warning: ['#613806', '#97590E', '#AF6D1C', '#FF8C00', '#F7A800', '#FFB400', '#FFDA80'],
   // Red/Pink
