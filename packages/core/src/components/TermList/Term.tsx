ebfa985bdfbb11419803e85bd52d19cec5a768c5
Kyle Schiller <kyleschiller@gmail.com>
2020-02-05 11:08:38 -0800

    migrate to useStyles

--- packages/core/src/components/TermList/Term.tsx
+++ packages/core/src/components/TermList/Term.tsx
@@ -1,34 +1,34 @@
 import React from 'react';
-import withStyles, { WithStylesProps } from '../../composers/withStyles';
+import useStyles, { StyleSheet } from '@airbnb/lunar/lib/hooks/useStyles';
 
 import Row from '../Row';
 import Spacing from '../Spacing';
 import Text from '../Text';
 
+export const styleSheet: StyleSheet = () => ({
+  dd: {
+    marginInlineStart: 0,
+  },
+});
+
 export type Props = {
-  /** Term label describing the value displayed. */
-  label: string;
   /** Detail links to go after Term label. */
   after?: React.ReactNode;
+  /** Term content describing the value displayed. */
+  children: NonNullable<React.ReactNode>;
   /** If enabled, detail links at aligned to the right end. */
   endAlign?: boolean;
+  /** Term label describing the value displayed. */
+  label: string;
   /** If enabled, term label is regular size instead of small. */
   regular?: boolean;
   /** If enabled, term label is uppercased. */
   uppercased?: boolean;
-  children: NonNullable<React.ReactNode>;
 };
 
-function Term({
-  label,
-  after,
-  endAlign,
-  regular,
-  uppercased,
-  children,
-  styles,
-  cx,
-}: Props & WithStylesProps) {
+export default function Term({ label, after, endAlign, regular, uppercased, children }: Props) {
+  const [styles, cx] = useStyles(styleSheet);
+
   return (
     <div className={cx(styles.term)}>
       <dt className={cx(styles.dt)}>
@@ -49,9 +49,3 @@ function Term({
     </div>
   );
 }
-
-export default withStyles(({ unit, font }) => ({
-  dd: {
-    marginInlineStart: 0,
-  },
-}))(Term);
--- packages/core/src/components/TermList/index.tsx
+++ packages/core/src/components/TermList/index.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import withStyles, { WithStylesProps } from '../../composers/withStyles';
+import useStyles, { StyleSheet } from '@airbnb/lunar/lib/hooks/useStyles';
 import Term from './Term';
 
 export { Term };
 
-export type Props = {
-  /** If enabled, terms are laid out horizontally. */
-  horizontal?: boolean;
-  children: NonNullable<React.ReactNode>;
-};
-
-function TermList({ horizontal, children, cx, styles }: Props & WithStylesProps) {
-  return (
-    <dl
-      className={cx(
-        styles.termList,
-        !horizontal && styles.termList_vertical,
-        horizontal && styles.termList_horizontal,
-      )}
-    >
-      {children}
-    </dl>
-  );
-}
-
-export default withStyles(({ unit, font }) => ({
+export const styleSheet: StyleSheet = ({ unit }) => ({
   termList: {
     marginBlockStart: 0,
     marginBlockEnd: 0,
@@ -45,4 +25,27 @@ export default withStyles(({ unit, font }) => ({
       },
     },
   },
-}))(TermList);
+});
+
+export type Props = {
+  /** Terms to be rendered inside the list. */
+  children: NonNullable<React.ReactNode>;
+  /** If enabled, terms are laid out horizontally. */
+  horizontal?: boolean;
+};
+
+export default function TermList({ horizontal, children }: Props) {
+  const [styles, cx] = useStyles(styleSheet);
+
+  return (
+    <dl
+      className={cx(
+        styles.termList,
+        !horizontal && styles.termList_vertical,
+        horizontal && styles.termList_horizontal,
+      )}
+    >
+      {children}
+    </dl>
+  );
+}
