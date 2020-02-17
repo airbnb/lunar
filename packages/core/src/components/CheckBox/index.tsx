5ee2e54f3bad57ea5a94844f40fecf0cb42679fa
Miles Johnson <miles.johnson@airbnb.com>
2020-02-17 11:24:09 +0000

    Update cb.

--- packages/core/src/components/CheckBox/index.tsx
+++ packages/core/src/components/CheckBox/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import uuid from 'uuid/v4';
 import { mutuallyExclusiveTrueProps } from 'airbnb-prop-types';
 import BaseCheckBox, { BaseCheckBoxProps } from '../private/BaseCheckBox';
@@ -13,63 +13,39 @@ export type CheckBoxProps<T extends string> = BaseCheckBoxProps<T> &
     topAlign?: boolean;
   };
 
-export type CheckBoxState = {
-  id: string;
+/** A controlled checkbox field. */
+function CheckBox<T extends string = string>(props: CheckBoxProps<T>) {
+  const { children, fieldProps, inputProps } = partitionFieldProps<T, CheckBoxProps<T>>(props);
+  const { topAlign, ...restProps } = inputProps;
+  const [id] = useState(() => uuid());
+
+  return (
+    <FormField
+      {...fieldProps}
+      inline
+      renderBeforeLabel
+      renderLargeLabel
+      stretchLabel
+      id={props.id || id}
+      hideLabel={fieldProps.hideLabel || inputProps.button}
+      renderFullWidth={inputProps.button}
+      topAlign={topAlign}
+    >
+      <BaseCheckBox<T> {...restProps} id={props.id || id} hideLabel={fieldProps.hideLabel}>
+        {children || (
+          <>
+            <Text bold>{fieldProps.label}</Text>
+            {fieldProps.labelDescription && <Text>{fieldProps.labelDescription}</Text>}
+          </>
+        )}
+      </BaseCheckBox>
+    </FormField>
+  );
+}
+
+CheckBox.propTypes = {
+  checked: stateProp,
+  indeterminate: stateProp,
 };
 
-/** A controlled checkbox field. */
-export default class CheckBox<T extends string = string> extends React.Component<
-  CheckBoxProps<T>,
-  CheckBoxState
-> {
-  static defaultProps = {
-    button: false,
-    checked: false,
-    children: null,
-    indeterminate: false,
-    topAlign: false,
-    value: '1',
-  };
-
-  static propTypes = {
-    checked: stateProp,
-    indeterminate: stateProp,
-  };
-
-  state = {
-    // Support for CheckBoxController
-    id: this.props.id || uuid(),
-  };
-
-  render() {
-    const { children, fieldProps, inputProps } = partitionFieldProps<T, CheckBoxProps<T>>(
-      this.props,
-    );
-    const { topAlign, ...restProps } = inputProps;
-    const { id } = this.state;
-    const { hideLabel } = fieldProps;
-
-    return (
-      <FormField
-        {...fieldProps}
-        inline
-        renderBeforeLabel
-        renderLargeLabel
-        stretchLabel
-        id={id}
-        hideLabel={fieldProps.hideLabel || inputProps.button}
-        renderFullWidth={inputProps.button}
-        topAlign={topAlign}
-      >
-        <BaseCheckBox<T> {...restProps} id={id} hideLabel={hideLabel}>
-          {children || (
-            <>
-              <Text bold>{fieldProps.label}</Text>
-              {fieldProps.labelDescription && <Text>{fieldProps.labelDescription}</Text>}
-            </>
-          )}
-        </BaseCheckBox>
-      </FormField>
-    );
-  }
-}
+export default CheckBox;
