eec7ae9c421e695035ffef99a1e59a40eafe6abe
Kyle Schiller <kyleschiller@gmail.com>
2020-02-24 14:50:19 -0800

    clean up tests

--- packages/core/test/components/DataTable.test.tsx
+++ packages/core/test/components/DataTable.test.tsx
@@ -22,64 +22,6 @@ import Button from '../../src/components/Button';
 import Checkbox from '../../src/components/CheckBox';
 import { STATUS_OPTIONS } from '../../src/components/DataTable/constants';
 
-type EditableTextRendererProps = Omit<RendererProps, 'theme' | 'zebra'> & {
-  value: string;
-};
-
-type EditableTextRendererState = {
-  value: string;
-};
-
-class InnerEditableTextRenderer extends React.Component<
-  EditableTextRendererProps,
-  EditableTextRendererState
-> {
-  state = {
-    value: this.props.value,
-  };
-
-  onEdit = (row: VirtualRow, keyName: string | number) => (
-    newVal: string,
-    event: React.SyntheticEvent<EventTarget>,
-  ) => {
-    const { onEdit } = this.props;
-    onEdit(row, keyName, newVal, event);
-
-    this.setState({
-      value: newVal,
-    });
-  };
-
-  render() {
-    const { editMode, row, keyName } = this.props;
-    const { value } = this.state;
-
-    return editMode ? (
-      <Input
-        hideLabel
-        label="Edit row"
-        name=""
-        value={value}
-        onChange={this.onEdit(row, keyName)}
-      />
-    ) : (
-      <Text>{value}</Text>
-    );
-  }
-}
-
-function EditableTextRenderer({ row, keyName, editMode, onEdit }: RendererProps) {
-  return (
-    <InnerEditableTextRenderer
-      editMode={editMode}
-      value={String(row.rowData.data[keyName])}
-      row={row}
-      keyName={keyName}
-      onEdit={onEdit}
-    />
-  );
-}
-
 const data: ParentRow[] = [
   {
     data: {
@@ -182,43 +124,11 @@ const columnMetadata = {
   },
 };
 
-const headerButtonClick = jest.fn();
-
-const editCallback = jest.fn();
-
-const selectCallback = jest.fn();
-
-const editCallbacks = {
-  name: editCallback,
-};
-
-const headerButtons = [
-  {
-    label: 'Always Displayed',
-    display: true,
-    displayEditMode: true,
-    onClick: headerButtonClick,
-  },
-  {
-    label: 'Extra Non Edit Button',
-    display: true,
-    displayEditMode: false,
-    onClick: headerButtonClick,
-  },
-  {
-    label: 'Extra Edit Mode Button',
-    display: false,
-    displayEditMode: true,
-    onClick: headerButtonClick,
-  },
-];
-
 const simpleProps = {
   data,
   width: 500,
   height: 300,
   expandable: true,
-  editable: true,
   tableHeaderLabel: 'My Great Table',
   showAllRows: true,
 };
@@ -249,11 +159,6 @@ const getTable = (wrapper: Enzyme.ShallowWrapper) => {
 const getHeaderFromStyledDataTable = (styledDataTable: Enzyme.ShallowWrapper) =>
   shallowWithStyles(styledDataTable.find(TableHeader).getElement(), true);
 
-const selectRow = (table: Enzyme.ReactWrapper<any, any>, row: number) => {
-  (getCheckbox(table, row).prop('onChange') as () => void)();
-  table.update();
-};
-
 const expandRow = (table: Enzyme.ReactWrapper<any, any>, row: number) => {
   getCaret(table, row)
     .childAt(0)
@@ -265,15 +170,7 @@ const ROW = 3;
 const PARENT_ROW = 5;
 const CHILD_ROW = 6;
 
-describe('<DataTable /> rows can be selected', () => {
-  it('should trigger callbacks on selection', () => {
-    const table = mountWithStyles(<DataTable {...simpleProps} selectCallback={selectCallback} />);
-
-    selectRow(table, ROW);
-
-    expect(selectCallback.mock.calls).toHaveLength(1);
-  });
-
+describe('<DataTable /> rows can be expanded', () => {
   it('should be expandable', () => {
     const table = mountWithStyles(<DataTable {...simpleProps} />);
 
@@ -297,46 +194,6 @@ describe('<DataTable /> rows can be selected', () => {
         .find(Text)
         .text(),
     ).toBe(data[5].data.name);
-  });
-
-  it('selecting the parent should select the children', () => {
-    const table = mountWithStyles(<DataTable {...simpleProps} />);
-
-    expandRow(table, PARENT_ROW);
-    selectRow(table, PARENT_ROW);
-
-    expect(getCheckbox(table, CHILD_ROW).prop('checked')).toBe(true);
-  });
-
-  it('selecting both children should select the parent', () => {
-    const table = mountWithStyles(<DataTable {...simpleProps} />);
-
-    expandRow(table, PARENT_ROW);
-    selectRow(table, CHILD_ROW);
-    selectRow(table, CHILD_ROW + 1);
-
-    expect(getCheckbox(table, PARENT_ROW).prop('checked')).toBe(true);
-  });
-
-  it('selecting the parent then deselecting child should deselect child', () => {
-    const table = mountWithStyles(<DataTable {...simpleProps} />);
-
-    expandRow(table, PARENT_ROW);
-    selectRow(table, PARENT_ROW);
-    selectRow(table, CHILD_ROW);
-
-    expect(getCheckbox(table, CHILD_ROW).prop('checked')).toBe(false);
-  });
-
-  it('Selecting the parent then deselecting both children should deselect the parent', () => {
-    const table = mountWithStyles(<DataTable {...simpleProps} />);
-
-    expandRow(table, PARENT_ROW);
-    selectRow(table, PARENT_ROW);
-    selectRow(table, CHILD_ROW);
-    selectRow(table, CHILD_ROW + 1);
-
-    expect(getCheckbox(table, PARENT_ROW).prop('checked')).toBe(false);
   });
 });
 
@@ -409,28 +266,6 @@ describe('<DataTable /> renders and sorts data', () => {
     expect(text).toBe('Dev Ops Danny');
   });
 
-  it('should sort data in Ascending order with selectedRowsFirst', () => {
-    const table = mountWithStyles(<DataTable selectedRowsFirst {...simpleProps} />);
-
-    selectRow(table, ROW);
-
-    const nameHeader = table.find('.ReactVirtualized__Table__headerColumn').at(NAME_COL - 1);
-    nameHeader.simulate('click');
-    table.update();
-
-    expect(
-      getCell(table, 1, NAME_COL)
-        .find(Text)
-        .text(),
-    ).toBe('Engineer Emma');
-
-    expect(
-      getCell(table, 2, NAME_COL)
-        .find(Text)
-        .text(),
-    ).toBe('Dev Ops Danny');
-  });
-
   it('should sort data in Ascending order through props', () => {
     const table = mountWithStyles(
       <DataTable {...simpleProps} sortByOverride="name" sortDirectionOverride="ASC" />,
@@ -448,19 +283,6 @@ describe('<DataTable /> renders and sorts data', () => {
       <DataTable {...simpleProps} sortByOverride="name" sortDirectionOverride="DESC" />,
     );
 
-    const text = getCell(table, 1, NAME_COL)
-      .find(Text)
-      .text();
-
-    expect(text).toBe('Product Percy');
-  });
-
-  it('should sort data in Descending order with selected rows first', () => {
-    const table = mountWithStyles(<DataTable selectedRowsFirst {...simpleProps} />);
-
-    const nameHeader = table.find('.ReactVirtualized__Table__headerColumn').first();
-    nameHeader.simulate('click');
-    nameHeader.simulate('click');
     const text = getCell(table, 1, NAME_COL)
       .find(Text)
       .text();
@@ -529,9 +351,7 @@ describe('<DataTable /> renders and sorts data', () => {
 
 describe('<DataTable /> renders column labels', () => {
   it('should render the correct column labels in sentence case', () => {
-    const table = getTable(
-      shallowWithStyles(<DataTable editable data={data} columnLabelCase="sentence" />),
-    );
+    const table = getTable(shallowWithStyles(<DataTable data={data} columnLabelCase="sentence" />));
     const columnLabels = table.childAt(0);
     const labels = ['Name', 'Job title', 'Tenure days', 'Menu', 'Cats', 'Log', 'Colspan'];
 
@@ -541,7 +361,7 @@ describe('<DataTable /> renders column labels', () => {
   });
 
   it('should not format labels by default', () => {
-    const table = getTable(shallowWithStyles(<DataTable editable data={data} />));
+    const table = getTable(shallowWithStyles(<DataTable data={data} />));
     const columnLabels = table.childAt(0);
     const labels = ['name', 'jobTitle', 'tenureDays', 'menu', 'cats', 'log', 'colspan'];
 
@@ -552,7 +372,7 @@ describe('<DataTable /> renders column labels', () => {
 
   it('should render the correct column labels in uppercase', () => {
     const table = getTable(
-      shallowWithStyles(<DataTable editable data={data} columnLabelCase="uppercase" />),
+      shallowWithStyles(<DataTable data={data} columnLabelCase="uppercase" />),
     );
     const columnLabels = table.childAt(0);
     const labels = ['NAME', 'JOB TITLE', 'TENURE DAYS', 'MENU', 'CATS', 'LOG', 'COLSPAN'];
@@ -563,9 +383,7 @@ describe('<DataTable /> renders column labels', () => {
   });
 
   it('should render the correct column labels in title case', () => {
-    const table = getTable(
-      shallowWithStyles(<DataTable editable data={data} columnLabelCase="title" />),
-    );
+    const table = getTable(shallowWithStyles(<DataTable data={data} columnLabelCase="title" />));
     const columnLabels = table.childAt(0);
     const labels = ['Name', 'Job Title', 'Tenure Days', 'Menu', 'Cats', 'Log', 'Colspan'];
 
@@ -606,101 +424,6 @@ describe('<DataTable /> renders column labels', () => {
     columnLabels.find(Text).forEach((node, idx) => {
       expect(node.prop('children')).toBe(labels[idx]);
     });
-  });
-});
-
-describe('<DataTable /> handles edits', () => {
-  const props: DataTableProps = {
-    data,
-    width: 500,
-    tableHeaderLabel: 'My Table',
-    editable: true,
-    editCallbacks,
-    renderers: {
-      name: EditableTextRenderer,
-    },
-  };
-
-  it('should be able to toggle edit mode off', () => {
-    const wrapper = shallowWithStyles(<StyledDataTable {...props} />);
-    const editButton = getHeaderFromStyledDataTable(wrapper).find(Button);
-
-    editButton.simulate('click');
-
-    const doneButton = getHeaderFromStyledDataTable(wrapper).find(Button);
-    doneButton.simulate('click');
-
-    expect(wrapper.state('editMode')).toBe(false);
-  });
-
-  it('should enable instant edit mode', () => {
-    const wrapper = shallowWithStyles(<StyledDataTable {...props} />);
-    const editButton = getHeaderFromStyledDataTable(wrapper).find(Button);
-
-    editButton.simulate('click');
-
-    const doneButton = getHeaderFromStyledDataTable(wrapper).find(Button);
-
-    expect(wrapper.state('editMode')).toBe(true);
-    expect(doneButton.find(Translate).prop('phrase')).toBe('Done');
-  });
-
-  it('should be editable', () => {
-    // @ts-ignore
-    const wrapper = mountWithStyles(<DataTable {...props} />);
-    const button = wrapper.find(TableHeader).find(Button);
-    button.simulate('click');
-    const grid = wrapper.find(Grid);
-    const row = grid.find('[aria-rowindex=1]');
-    const col = row.find('[aria-colindex=1]');
-    const input = col.find(Input).find(FormInput);
-
-    const event = {
-      currentTarget: {
-        value: 'foo',
-      },
-    };
-
-    input.simulate('change', event);
-    input.simulate('click');
-
-    expect(
-      wrapper
-        .find(Grid)
-        .find('[aria-rowindex=1]')
-        .find('[aria-colindex=1]')
-        .find(Input)
-        .prop('value'),
-    ).toBe(data[0].data.name);
-  });
-
-  it('should be editable without instant edit', () => {
-    // @ts-ignore
-    const wrapper = mountWithStyles(<DataTable {...props} instantEdit={false} />);
-    const button = wrapper.find(TableHeader).find(Button);
-    button.simulate('click');
-    const grid = wrapper.find(Grid);
-    const row = grid.find('[aria-rowindex=1]');
-    const col = row.find('[aria-colindex=1]');
-    const input = col.find(Input).find(FormInput);
-
-    const event = {
-      currentTarget: {
-        value: 'foo',
-      },
-    };
-
-    input.simulate('change', event);
-    input.simulate('click');
-
-    expect(
-      wrapper
-        .find(Grid)
-        .find('[aria-rowindex=1]')
-        .find('[aria-colindex=1]')
-        .find(Input)
-        .prop('value'),
-    ).toBe(data[0].data.name);
   });
 });
 
