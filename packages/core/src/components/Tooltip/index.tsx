38d13c93c328f92712898e59a63b5dbe6d0feded
Kyle Schiller <kyleschiller@gmail.com>
2020-02-24 16:14:30 -0800

    revert tooltip

--- packages/core/src/components/Tooltip/index.tsx
+++ packages/core/src/components/Tooltip/index.tsx
@@ -222,7 +222,7 @@ export class Tooltip extends React.Component<TooltipProps & WithStylesProps, Too
             textAlign: align,
           })}
         >
-          <div className={cx(styles.notchedBoxContainer)}>
+          <div className={cx(styles.shadow)}>
             <NotchedBox
               inverted={!inverted}
               notchOffset={notchOffset[align as keyof StyleStruct]}
