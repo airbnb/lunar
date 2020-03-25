a0cb6ad7d69c086b6c6895251669a04425082fe8
Miles Johnson <miles.johnson@airbnb.com>
2020-03-24 17:00:19 -0700

    internal: Fix emojis.

--- .storybook/bootstrap.js
+++ .storybook/bootstrap.js
@@ -1,14 +1,14 @@
 import 'airbnb-js-shims';
 import { stripHexcode } from 'emojibase';
-import Lunar from '@airbnb/lunar';
+import Lunar from '@airbnb/lunar/src'; // src required because of aliases
 import { THEME } from './constants';
 
 Lunar.initialize({
   name: 'Lunar',
   emojiCDN: (hexcode, large) =>
-    `https://cdn.jsdelivr.net/emojione/assets/4.5/png/${large ? 64 : 32}/${stripHexcode(
-      hexcode,
-    ).toLowerCase()}.png`,
+    `https://cdn.jsdelivr.net/gh/joypixels/emoji-assets@latest/png/${
+      large ? 64 : 32
+    }/${stripHexcode(hexcode).toLowerCase()}.png`,
   logger: console.log,
   theme: THEME,
 });
