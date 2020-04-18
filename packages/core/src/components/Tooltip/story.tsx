5249f58c84091c6c410546455d0b374151e9e0ad
Chris Williams <chris.williams@airbnb.com>
2020-04-17 18:08:04 -0700

    docs(Tooltip): Add accessibilityLabel example

--- packages/core/src/components/Tooltip/story.tsx
+++ packages/core/src/components/Tooltip/story.tsx
@@ -214,3 +214,33 @@ export function toggleWithClick() {
 toggleWithClick.story = {
   name: 'Toggle tooltip on click',
 };
+
+export function withAccessibilityLabel() {
+  return (
+    <>
+      <Spacing bottom={1}>
+        <Tooltip
+          accessibilityLabel="Override accessibility content"
+          content="I have an aria-labelledby instead of aria-label"
+        >
+          I have an{' '}
+          <Text bold inline>
+            aria-label
+          </Text>{' '}
+          instead of aria-labelledby
+        </Tooltip>
+      </Spacing>
+      <Tooltip content="I have an aria-label instead of aria-labelledby">
+        I have an{' '}
+        <Text bold inline>
+          aria-labelledby
+        </Text>{' '}
+        instead of aria-label
+      </Tooltip>
+    </>
+  );
+}
+
+withAccessibilityLabel.story = {
+  name: 'With accessibility label',
+};
