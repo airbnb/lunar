f8fcf5fd49eeb9bf6d39fb83172ae1f063eaab35
Tyler Geonetta <tyler.geonetta@airbnb.com>
2020-04-23 15:48:13 -0700

    Fix type

--- packages/core/src/components/ErrorBoundary/story.tsx
+++ packages/core/src/components/ErrorBoundary/story.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ErrorBoundary from '.';
+import { ErrorObject } from '../..';
 
 function TestComponent() {
   const error = new Error('This was thrown from a child.');
-  (error as unknown).error_id = '6db2d1a0275c97134535b4681914f8b6d';
+  (error as ErrorObject).error_id = '6db2d1a0275c97134535b4681914f8b6d';
   throw error;
 
   // eslint-disable-next-line
