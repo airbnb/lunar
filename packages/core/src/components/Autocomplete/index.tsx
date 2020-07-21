40d52f757b96746b1779ff2418a4438838313591
xinyu.liu <xinyu.liu@airbnb.com>
2020-07-21 15:10:20 +0800

    fix(Autocomplete): code format

--- packages/core/src/components/Autocomplete/index.tsx
+++ packages/core/src/components/Autocomplete/index.tsx
@@ -446,7 +446,19 @@ export default class Autocomplete<T extends Item = Item> extends React.Component
   }
 
   getInputProps(props: AutocompleteProps<T> & { hasPrefix: boolean; hasSuffix: boolean }) {
-    const { disabled, invalid, name, optional, placeholder, onBlur, onFocus, small, large, hasPrefix, hasSuffix } = props;
+    const {
+      disabled,
+      invalid,
+      name,
+      optional,
+      placeholder,
+      onBlur,
+      onFocus,
+      small,
+      large,
+      hasPrefix,
+      hasSuffix,
+    } = props;
     const { id } = this.state;
 
     // Should match the props passed within `Input`
