9a829b2fcb5cedf267e77e68b72c1434d06b2f0a
Elizabeth Moore <elizabeth.amoore@airbnb.com>
2020-07-30 21:04:50 -0700

    Add story

--- packages/core/src/components/Link/story.tsx
+++ packages/core/src/components/Link/story.tsx
@@ -115,3 +115,18 @@ export function boldText() {
 boldText.story = {
   name: 'Bold text.',
 };
+
+export function truncatedText() {
+  return (
+    <Link block truncated>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam leo erat, lacinia nec porttitor
+      sed, mollis sed nibh. Nam porta sit amet risus quis interdum. Sed feugiat lorem vitae augue
+      blandit, sed mollis mi laoreet. Donec auctor, enim eget tempus auctor, est lorem laoreet nisi,
+      a rutrum dolor quam eget mi.
+    </Link>
+  );
+}
+
+truncatedText.story = {
+  name: 'Truncated text.',
+};
